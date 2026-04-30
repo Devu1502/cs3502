fc2ce88773fde33a80c0813b0ac0309f93280260
Devananda Sreekanth <nanda@Devanandas-MacBook-Pro.local>
2026-04-30 00:08:15 -0400

    file listing implemented

--- Project3_FileSystem/src/main.py
+++ Project3_FileSystem/src/main.py
@@ -1,15 +1,26 @@
 import tkinter as tk
+from file_ops import list_files
+import os
 
 def main():
     root = tk.Tk()
     root.title("File Manager")
     root.geometry("600x400")
 
-    label = tk.Label(root, text="My File System", font=("Arial", 16))
-    label.pack(pady=10)
+    current_path = os.getcwd()
+
+    label = tk.Label(root, text=f"Path: {current_path}", font=("Arial", 10))
+    label.pack(pady=5)
+
+    listbox = tk.Listbox(root, width=80)
+    listbox.pack(pady=10)
+
+    files = list_files(current_path)
+
+    for file in files:
+        listbox.insert(tk.END, file)
 
     root.mainloop()
 
 if __name__ == "__main__":
     main()
-    
